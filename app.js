781e1756db241ed24b7a8e0334ce35496b358b3e
Jiacheng Wei <wjc0326@seas.upenn.edu>
2022-09-03 00:35:43 -0400

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
